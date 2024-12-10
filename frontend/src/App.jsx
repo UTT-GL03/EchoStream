4cd2ccff06853e2f41fc88b71300a1c8244a1907
Arthur Paris <arthur.paris999@gmail.com>
2024-12-10 15:17:15 +0100

    passage de la barre de musique dans un fichier à part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import AudioPlayer from './AudioPlayer';
+import PlayerBar from './PlayerBar';
 
 const MusicSearchApp = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -110,16 +111,7 @@ const MusicSearchApp = () => {
           )}
         </div>
       </div>
-
-      {randomSong && (
-        <footer className="fixed bottom-0 left-0 w-full p-4 text-center">
-          <div className="bg-white p-4 rounded-lg shadow hover:shadow-md transition-shadow">
-            <h2 className="text-lg font-semibold text-gray-800">Musique à découvrir !</h2>
-            <p className="text-gray-600">{randomSong.title} - {randomSong.artist}</p>
-            <AudioPlayer song={randomSong} />
-          </div>
-        </footer>
-      )}
+      <PlayerBar randomSong={randomSong} />
     </div>
   );
 };
