b29de7c048f252828f762c8898e6869d65f771d9
rocharrond <romain.charrondiere@utt.fr>
2024-12-10 15:31:54 +0100

    Ajout d'une page pour afficher les musiques et artistes d'une catégorie spécifique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MusicSearchApp from './MusicSearchApp';
+import CategoryPage from './CategoryPage';
 
 const App = () => {
-  return <MusicSearchApp />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MusicSearchApp />} />
+        <Route path="/category/:genre" element={<CategoryPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
